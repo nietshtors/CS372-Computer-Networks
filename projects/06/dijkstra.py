04fb8bc16c415b63a5b0786ac3702948b0a57a42
Seth Rothstein <sethmrothstein@gmail.com>
2024-12-11 23:22:32 -0800

    Finished dijkstra portion of project 6

--- projects/06/dijkstra.py
+++ projects/06/dijkstra.py
@@ -3,6 +3,94 @@ import json
 import math  # If you want to use math.inf for infinity
 
 
+class Graph:
+    def __init__(self, routers: dict):
+        self.routers = routers
+        self.nodes = [r for r in routers.keys()]
+        for r in self.nodes:
+            for x in routers[r]['connections'].keys():
+                if x not in self.nodes: self.nodes.append(x)
+        self.adjmat = [[0 for _ in range(len(self.nodes))] for _ in range(len(self.nodes))]
+        self.setup()
+        
+    def __str__(self):
+        s = ''
+        for i in range(len(self.nodes)):
+            for j in range(len(self.nodes)):
+                s += f'{self.adjmat[i][j]:4d}'
+            s += '\n'
+        return s
+
+    def setup(self):
+        for r in self.nodes:
+            row = self.get_index(r)
+            connections = self.routers[r]['connections']
+            for c in connections.keys():
+                column = self.get_index(c)
+                weight = connections[c]['ad']
+                self.adjmat[row][column] = weight
+                
+    def get_index(self, ip):
+        return self.nodes.index(ip)
+    
+    def dist(self, src, dest):
+        return self.adjmat[self.get_index(src)][self.get_index(dest)]
+    
+    def neighbors(self, src, queue):
+        return [n for n in self.nodes if self.adjmat[self.get_index(src)][self.get_index(n)] != 0 and n in queue]
+                
+
+def ipv4_to_value(ipv4_addr) -> int:
+    """
+    Convert a dots-and-numbers IP address to a single 32-bit numeric
+    value of integer type. Returns an integer type.
+    """
+
+    arr = ipv4_addr.split('.')
+    arr.reverse()
+    val = 0
+    for i in range(4):
+        val |= int(arr[i]) << 8 * i
+    return val
+
+
+def get_subnet_mask_value(slash) -> int:
+    """
+    Given a subnet mask in slash notation, return the value of the mask
+    as a single number of integer type. The input can contain an IP
+    address optionally, but that part should be discarded.
+    """
+
+    num = int(slash.split('/')[1])
+    return 0xffffffff & ~((1 << (32-num))-1)
+
+
+def ips_same_subnet(ip1: str, ip2: str, slash: str) -> bool:
+    """
+    Given two dots-and-numbers IP addresses and a subnet mask in slash
+    notataion, return true if the two IP addresses are on the same
+    subnet.
+    """
+
+    ip1 = ipv4_to_value(ip1)
+    ip2 = ipv4_to_value(ip2)
+    subnet_mask = get_subnet_mask_value(slash)
+    return ip1 & subnet_mask == ip2 & subnet_mask
+
+
+def find_router_for_ip(routers: str, ip: str):
+    """
+    Search a dictionary of routers (keyed by router IP) to find which
+    router belongs to the same subnet as the given IP.
+
+    Return None if no routers is on the same subnet as the given IP.
+    """
+
+    for router, d in routers.items():
+        if ips_same_subnet(ip, router, d['netmask']): return router
+    return None
+
+
 def dijkstras_shortest_path(routers, src_ip, dest_ip):
     """
     This function takes a dictionary representing the network, a source
@@ -57,8 +145,46 @@ def dijkstras_shortest_path(routers, src_ip, dest_ip):
     for madness.
     """
 
-    # TODO Write me!
-    pass
+    def min_(to_visit, dist):
+        distance = math.inf
+        node = ''
+        for n in to_visit:
+            if dist[n] <= distance:
+                node = n
+                distance = dist[n]
+        return node
+    
+    src_r = find_router_for_ip(routers, src_ip)
+    dest_r = find_router_for_ip(routers, dest_ip)    
+    if src_r == dest_r: return []
+    
+    graph = Graph(routers)
+    dist = {n: math.inf for n in graph.nodes}
+    parent = {n: None for n in graph.nodes}
+    dist[src_r] = 0
+    
+
+    to_visit = graph.nodes.copy()
+    while to_visit:
+        current = min_(to_visit, dist)
+        to_visit.remove(current)
+
+        for neighbor in graph.neighbors(current, to_visit):
+            alt = dist[current] + graph.dist(current, neighbor)
+            if alt < dist[neighbor]:
+                dist[neighbor] = alt
+                parent[neighbor] = current
+
+    current = dest_r
+    path = []
+    while current != src_r:
+        path.append(current)
+        current = parent[current]
+    path.append(src_r)
+    path.reverse()
+    
+    return path
+        
 
 #------------------------------
 # DO NOT MODIFY BELOW THIS LINE
